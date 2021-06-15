5c10ffb5caecd08c5994b924edd26dfe3d495819
ypnors <godhype@gmail.com>
2021-06-15 18:01:10 +0800

    6/15 1800

---
+++ src/main/java/com/fju/Player.java
@@ -0,0 +1,18 @@
+package com.fju;
+
+import java.util.*;
+interface Player {
+    final static String[][] cname={{"梅花","方塊","紅心","黑桃"},
+            {"Club","Diamond","Hearts","Spades"},
+            {"花","塊","心","桃"},
+            {"C","D","H","S"}};
+    /**** method 用來show出第幾張牌 ******/
+    abstract public String show(int id);
+    abstract public int card_num();
+    /**** 發牌的方法設定為abstract ******/
+    abstract public void set_card(int x);
+    /****** 要不要再發牌的方法設定為 abstract *******/
+
+    abstract public boolean isgo(); //決定要不要再要牌
+    abstract public int get_card_value();
+}
